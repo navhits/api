be80edc0b597ea31cb264a62d5e9fb88e0bd6677
Naveen S <naveen2aug@gmail.com>
2022-01-04 12:15:12 +0530

    Updated test cases and workflow (#3)
    * Updated test cases
    
    * ci/cd cleanup and updated

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,4 +15,15 @@ def test_info():
 def test_storage():
     response = client.get("/storage?path=images/naveen.jpeg")
     assert response.status_code == 200
-    
+
+def test_storage_fail():
+    response = client.get("/storage?path=images/invalid.jpeg")
+    assert response.status_code == 404
+
+def test_api_health():
+    response = client.get("/health/api")
+    assert response.status_code == 200
+
+def test_db_health():
+    response = client.get("/health/db")
+    assert response.status_code == 200
